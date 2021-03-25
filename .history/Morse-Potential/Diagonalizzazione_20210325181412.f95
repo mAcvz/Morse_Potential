940c95282cfb095fc9322458e6b49c033c26f461
mAcvz <marco.cavazza99@gmail.com>
2021-03-25 18:48:26 +0100

    Aumento leggibilità Scrittura su terminale

---
+++ .history/Morse-Potential/Diagonalizzazione_20210325181412.f95
@@ -0,0 +1,87 @@
+PROGRAM Diagonalizzazione
+    ! 
+    ! Programma che prende in ingresso L,N,M,alpha e richiama la funzione lapack DPTEQR(...)  per 
+    ! risolvere l'EQ. di Schrodinger unidimensionale per il potenziale di Morse. Il problema
+    ! viene affrontato determinando A.Valori e A.Vettori (riscalati) di una matrice tridiagonale. 
+    ! Il programma salva su file.txt i risultati ottenuti
+    ! 
+    ! VARIABLES dichiarate in MODULE Dichiazione
+    ! SUBROUTINE di controllo in MODULE Control
+    ! SUBROUTINE per salvataggio su file in MODULE Scrittura_File
+    !
+    ! PER MAGGIORI INFORMAZIONI CONTROLLARE DOCUMENTAZIONE DI OGNI "MODULE"
+    !
+    USE Dichiarazione
+    USE Scrittura_File
+    USE Control
+    !
+    IMPLICIT NONE    
+    !
+    !
+    ! Lettura RANGE, N°PUNTI, N° Autovalori & Autovettori, alpha
+    !
+    WRITE(*,*) NEW_LINE("A") // "PROGRAMMA:  " // NEW_LINE("A")
+    !
+    WRITE(*,*) "POTENZIALE DI MORSE: CALCOLO AUTO VALORI & AUTO FUNZIONI " //NEW_LINE("A")
+    !
+    WRITE(*,*) "Lettura parametri di input da file: ", FileInput, NEW_LINE("A")
+    !
+    OPEN(UNIT=UnitRead,FILE = FileInput,IOSTAT=ioerrInput) 
+    !
+    READ(UNIT=UnitRead,FMT = *,IOSTAT = ioerrInput)
+    !
+    READ(UNIT=UnitRead,FMT = *,IOSTAT = ioerrInput) N,L,M,alpha 
+    !
+    CLOSE(UNIT=UnitRead,IOSTAT=ioerrInput)
+    !
+    WRITE(*,FMT="(i6,i4,i2,f5.3)")(N,L,M,alpha )
+    !
+    ! CONTROLLO INSERIMENTO
+    CALL Control_Ins()
+    !
+    WRITE(*,*)"Calcolo ..."
+    !
+    ! ALLOCAZIONE VARIABILI DI LAVORO
+    !
+    LDZ=N
+    !
+    ALLOCATE(E(N-1))
+    ALLOCATE(D(N))
+    ALLOCATE(Work(4*N))
+    ALLOCATE(Z(LDZ,N))
+    !
+    !
+    
+    ! CALCOLO: passo di avanzamento h e creazione vettori E e D
+    h = L/(N-1)
+    E = -1. / h**2 
+    !
+    !V(i) = (1 - exp(-(alpha)*(-L/2. + h*(i-1)) ))**2
+    DO i = 1,N 
+        D(i) = (2./h**2 + (1 - exp(-(alpha)*(-L/2. + h*(i-1)) ))**2)
+    END DO 
+    !
+    !
+    ! Funzione lapack: per determinazione di autovalori & auotovettori : controllare documentazione
+    ! http://www.netlib.org/lapack/explore-html/d0/d2f/group__double_p_tcomputational_ga03d834df95ce593c02831f77602cfa7d.html#ga03d834df95ce593c02831f77602cfa7d
+    CALL DPTEQR(COMPTZ, N, D, E, Z, LDZ, Work, Info)
+    !
+    !
+    ! CONTROLLO ESECUZIONE DPTEQR()
+    CALL Control_DPTEQR()
+    !
+    !
+    ! SCRITTURA AUTOVAL
+    CALL Scrittura_File_Autoval()
+    !
+    !
+    ! SCRITTURA AUTOVETTORI 
+    CALL Scrittura_File_Autovet()
+    !
+    !
+    ! SCRITTURA FILE GRIGLIA PUNTI
+    CALL Scrittura_File_Griglia()
+    !
+    !
+    !
+    END PROGRAM Diagonalizzazione
---
+++ .history/Morse-Potential/Scrittura_File_20210325175731.f95
@@ -0,0 +1,126 @@
+ MODULE Scrittura_File
+    !
+    ! Modulo contenente 3 SUBROUTINE dedicate alla scrittura su file: 
+    !
+    !
+    ! 1) SUBROUTINE  Scrittura_File_Autoval (...)
+    ! genera 1 file.txt su cui salvare A.valori 
+    !
+    !
+    ! 2) SUBROUTINE Scrittura_File_Autovet (...)
+    ! genera M file.txt su M unità differenti in ordine progressivo su cui salvare gli A.vettori 
+    ! valutati sui punti della griglia (con passo N/P)
+    !
+    !
+    ! 3) SUBROUTINE Scrittura_File_Griglia
+    ! genera 1 file.txt su cui salvare i punti della griglia con passo N/P
+    !
+    !
+    USE Dichiarazione   
+    !
+    IMPLICIT NONE 
+    !
+    CONTAINS 
+    !
+    SUBROUTINE Scrittura_File_Autoval()      !!! scrittura AUTOVALORI
+        !
+        IMPLICIT NONE 
+        !
+        !
+        !
+        OPEN(UNIT=UnitAVAL,FILE=FileName_Autoval,IOSTAT=ioerr)
+        !
+        WRITE(UNIT=UnitAVAL,FMT='(a22)' ,IOSTAT=ioerr) "Indice     Autovalore"
+        !
+        WRITE(UNIT=UnitAVAL,FMT= FMTwrite_AV,IOSTAT=ioerr)(i,"    ",D(N-i)/2,i=0,M-1)   ! stampa A.Valori su file
+        !
+        WRITE(*,"(A)") NEW_LINE("A")//"Anteprima autovalori:"//NEW_LINE("A")
+        WRITE(*,FMT="(i3,f15.10)",IOSTAT=ioerr)(i,D(N-i)/2,i=0,M-1)                  ! stampa A.Valori su display
+        !
+        CLOSE(UNIT=UnitAVAL,IOSTAT=ioerr)
+        !
+        control_1: IF (ioerr .NE. 0) THEN
+            !
+            PRINT "('ESITO SCRITTURA FILE AUTOVALORI : ioerr = ',i1,' ERRORE')",ioerr   ! Esito operazione
+            !
+        ELSE
+            WRITE(*,*) NEW_LINE("A")//"AUTOVALORI STAMPATI CORRETAMENTE SUL FILE: " ,FileName_Autoval ! Esito operazione
+        END IF control_1
+        !
+    END SUBROUTINE Scrittura_File_Autoval
+    !
+    !
+    !
+    SUBROUTINE Scrittura_File_Autovet()  !!! scrittura AUTOVETTORI
+        !
+        IMPLICIT NONE 
+        !
+        !
+        NameFileDO : DO k = 0, M-1                                                                 ! ciclo per stampa su file progressivi
+        !
+        ! 
+        UnitAVET  = UnitAVET  + k 
+        !
+        control_2: IF (k < 10) THEN
+            !
+            WRITE(file_dati_autovet,FMT_fileA ) name_start,k,".txt"                                ! Generazione  nome fileA.txt
+            !
+        ELSE  
+            !
+            WRITE(file_dati_autovet,FMT_fileB) name_start,k,".txt"                                 ! Generazione  nome fileB.txt
+            !
+        END IF control_2
+        !
+        file_dati_autovet = ADJUSTL(file_dati_autovet) 
+        !
+        OPEN(UNIT = UnitAVET , FILE = file_dati_autovet, IOSTAT = ioerr) 
+        !
+        WRITE(UNIT = UnitAVET ,FMT= FMTwrite,IOSTAT=ioerr)(Z(i,LDZ-k),i=1,N,N/P)                    ! Scrittura A.Vettori                  
+        !
+        CLOSE(UNIT = UnitAVET ,IOSTAT=ioerr)
+        !
+        control_3: IF (ioerr .NE. 0) THEN
+            !
+            PRINT"('ESITO SCRITTURA ',i4,'° AUTOVETTORE: ioerr = ',i1,' ERRORE')",k+1,ioerr        ! Esito scrittura
+            !
+        END IF control_3
+        !
+        END DO NameFileDO
+        !
+        !
+        !
+        control_4: IF (ioerr .EQ. 0)  THEN 
+        !
+            WRITE(name_end,"(a15,a19,i2)") name_start,"i.txt   i = 0,1 ...",M-1
+            WRITE(*,*) NEW_LINE("A")//"AUTOVETTORI STAMPATI CORRETAMENTE SUI FILE: ", name_end                    ! Esito operazione
+        ELSE 
+        !
+            WRITE(*,*) "AUTOVETTORI NON SALVATI CORRETTAMENTE"                                     ! Esito scrittura
+        !
+        END IF control_4
+        !
+    END SUBROUTINE Scrittura_File_Autovet
+        !
+        !
+        !
+    SUBROUTINE  Scrittura_File_Griglia()   !!! scrittura GRIGLIA
+        !
+        IMPLICIT NONE 
+        !
+        !
+        !
+        OPEN(UNIT=UnitGRIGLIA,FILE=File_Punti_Valutazione,IOSTAT=ioerr)
+        !
+        WRITE(UNIT=UnitGRIGLIA,FMT= FMTwrite,IOSTAT=ioerr)(-L/2. + h*(i-1),i=1,N,N/P)
+        !
+        CLOSE(UNIT=UnitGRIGLIA,IOSTAT=ioerr)
+        ! 
+        control_5: IF (ioerr .NE. 0) THEN
+            PRINT "('ESITO SCRITTURA PUNTI DI VALUTAZIONE : ioerr = ',i1,' ERRORE')",ioerr         ! Esito scrittura
+        END IF control_5
+        !
+    END SUBROUTINE Scrittura_File_Griglia
+!
+!
+!
+END MODULE Scrittura_File
