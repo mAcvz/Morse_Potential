76adbbda90d7964e68d1644b886fa3b2ab98a0fc
mAcvz <256833@studenti.unimore.it>
2021-04-12 11:26:06 +0200

    12-04-2021
    Co-authored-by: LUCA PEDRONI <255741@unimore.it>

--- ARMONIC_POTENTIAL (FFTW)/main_legacy.f95
+++ ARMONIC_POTENTIAL (FFTW)/main_legacy.f95
@@ -1,10 +1,12 @@
-program  main_2_0    
+program  main_2_0  
+!
     implicit none 
     include "fftw3.f"
     integer, parameter :: dp = kind(0.0D0) 
+    real(kind=dp), parameter :: pi = acos(-1.d0)
     integer :: N,F,i,j,INFO,LWORK ,k,kp
     character, parameter :: JOBZ = 'V', UPLO = 'U'
-
+    !
     real(kind=dp) :: h, L !,tic, toc 
     real(kind=dp),dimension(:),allocatable :: x ,W,WORK
     complex(kind=dp),dimension(:),allocatable :: in,out
@@ -26,7 +28,6 @@ program  main_2_0
     allocate(Ham(N,N))
     allocate(W(N))
     allocate(WORK(LWORK))
-    
     !
     h = L/dble(N)
     do i=1, N
@@ -34,60 +35,63 @@ program  main_2_0
         in(i) = x(i)**2
     end do
     !call cpu_time(tic)    
+    !UTILIZZO FFTW3j
     call dfftw_plan_dft_1d(plan,N,in,out,FFTW_forward,fftw_estimate)
     call dfftw_execute(plan,in,out)
     call dfftw_destroy_plan(plan)
-    out = out/dble(N)
+    out = out/dble(N)        !????
+
 
     ! call cpu_time(toc)
     open(unit=2,file='file_output.dat')
     write(unit=2,fmt="(i6,f30.15,a3,f30.15,a2)")(i,real(out(i)),"  ",(aimag(out(i)))," i",i=1,N)
     close(unit=2)
     ! write(*,*) toc - tic 
+
+    !COSTRUZIONE MATRICE V
     do i=0,N-1
         do j = 1, N-i 
             V(j,j+i)= real(out(i+1))  
            ! if (i .ne. 0 ) then 
-           !     V(j+i,j)= real(out(i+1))     !RIEMPIAMO SOLO SOPRA
+           !     V(j+i,j) = real(out(i+1))     !RIEMPIAMO SOLO SOPRA
            ! end if  
-        end do
+        end do  
     end do 
+    !  k = i *(pi/L)     
     !
-    Ham = 0.d0
+    
+    Ham = 0.d0   
     do i=1,N
-        Ham(i,i) = i**2
+        Ham(i,i) = (dble(i)*pi/L)**2
     end do 
     
     open(unit=3,file='riga_v.txt')
     write(unit=3,fmt="(i4,f15.5)")(i,real(V(1,i)),i=1,N)
     close(unit=3)
-
-    
-   
+    !METODO ANALITICO 
     do k = 1, N 
         do kp = 1, N 
             if (kp > k) then       !QUI CI VA .ne. PER COSTRUIRE TUTTA LA MATRICE V_TRUE
-            
-                V_true(k,kp) = ((((L*(kp-k))**2 -2)*sin(L*(kp-k))) + (2*(kp-k)*L*cos(L*(kp-k)))) / (L*(kp-k)**3)
+                V_true(k,kp) = ((((pi*(kp-k))**2 -2)*sin(pi*(kp-k))) + (2*(kp-k)*pi*cos(pi*(kp-k)))) / (((pi*(kp-k))**3)/(L**2))
             end if 
        end do 
     end do 
     do i = 1, N
         V_true(i,i) = (L**2)/3
     end do 
-
-
-    open(unit=4,file='colonna_v_true.txt')
-    write(unit=4,fmt="(i4,f15.5,f15.5)")(i,V_true(i,1),V_true(i,6),i=1,N)
+    !    
+    open(unit=4,file='riga_v_true.txt')
+    write(unit=4,fmt="(i4,f15.5,f15.5)")(i,V_true(1,i),V_true(2,i),i=1,N)
     close(unit=4)
+    !
+    
+    !Ham = Ham + V
+    Ham = Ham + V_true
 
-    Ham = Ham + V 
-    !Ham = Ham + V_true
 
     call dsyev(JOBZ,UPLO,N,Ham,N,W,WORK,LWORK,INFO)
     print*, WORK(1)
     print*, INFO 
-    write(*,fmt="(i4,f15.5)")(i,W(i)/2,i=1,5)
-
-    
+    write(*,fmt="(i4,f15.5)")(i,W(i)/2 ,i=1,5)
+!  
 end program  main_2_0
